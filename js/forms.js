251dbfe20d2848c55165a1b472009659279a282e
Scott Laughlin <scott.laughlin15@gmail.com>
2016-09-25 19:32:52 -0400

    Added a web form that sends me an e-mail when someone is interested

--- js/forms.js
+++ js/forms.js
@@ -2,13 +2,6 @@
  * Created by scott on 2016-09-25.
  */
 $(document).ready(function(){
-  alert("jquery is working");
-  $('button').click(function(){
- $("button").removeClass("selected");
-  $(this).addClass("selected");
-
-  });
-
 
 
 });//end ready
